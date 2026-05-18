a13f08f9ef4ba688c2bbd1b76455eb0a3bb5c24b
taegyung <taegyung.e@gmail.com>
2026-05-19 00:50:18 +0900

    chore: 여행 등록 기능 일부 수정

--- src/features/trips/tripCreate/ui/TripCreateForm.tsx
+++ src/features/trips/tripCreate/ui/TripCreateForm.tsx
@@ -2,19 +2,22 @@
 
 import { useRouter } from 'next/navigation';
 
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'sonner';
 
 import { createTrip } from '@/entities/trips/api/trips.api';
+import { TRIPS_QUERIES } from '@/entities/trips/api/trips.queries';
 import { TripValuesType } from '@/entities/trips/type';
 import TripForm from '@/features/trips/tripForm/ui/TripForm';
 import { PATH } from '@/shared/constants/path';
 
 export default function TripCreateForm() {
   const router = useRouter();
+  const queryClient = useQueryClient();
   const { mutateAsync } = useMutation({
     mutationFn: createTrip,
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: TRIPS_QUERIES.lists() });
       router.push(PATH.global.trips.list);
     },
     onError: (error) => {
@@ -22,7 +25,10 @@ export default function TripCreateForm() {
     },
   });
 
-  const onSubmit = (formData: TripValuesType) => mutateAsync(formData);
+  const onSubmit = async (formData: Partial<TripValuesType>) => {
+    const submitData = formData as TripValuesType;
+    await mutateAsync(submitData);
+  };
 
   return <TripForm onSubmit={onSubmit} />;
 }
