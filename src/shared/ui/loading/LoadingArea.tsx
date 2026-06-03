78130d30b4c0c8e9eba6c4dc79a02c0538885e6f
taegyung <taegyung.e@gmail.com>
2026-06-04 01:51:30 +0900

    chore: 로딩 컴포넌트 및 파일 위치 수정

---
+++ src/shared/ui/loading/LoadingArea.tsx
@@ -0,0 +1,9 @@
+import Loading from '@/shared/ui/loading/Loading';
+
+export default function LoadingArea() {
+  return (
+    <div className='flex-full flex flex-1 items-center justify-center'>
+      <Loading />
+    </div>
+  );
+}
