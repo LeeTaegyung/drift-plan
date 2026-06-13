ea995a1839888673f65f794e0e305c24ce742206
taegyung <taegyung.e@gmail.com>
2026-06-14 01:53:46 +0900

    feat: 기타 카드 UI 및 데이터 바인딩

--- src/features/trips/tripSchedule/ui/TripScheduleContent/TripScheduleDetail/EtcDetailCard.tsx
+++ src/features/trips/tripSchedule/ui/TripScheduleContent/TripScheduleDetail/EtcDetailCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { EtcCardFormValues } from '@/features/trips/tripSchedule/model/scheduleForm.schema';
 
 interface Props {
@@ -5,5 +7,18 @@ interface Props {
 }
 
 export default function EtcDetailCard({ detail }: Props) {
-  return <div>기타 카드</div>;
+  return (
+    <>
+      {detail.photo_url && (
+        <Image
+          src={detail.photo_url as string}
+          alt={'사진'}
+          width={500}
+          height={500}
+          style={{ width: '100%', maxWidth: '500px', height: 'auto' }}
+          className='mt-1'
+        />
+      )}
+    </>
+  );
 }
