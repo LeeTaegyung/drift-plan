d3ebb5dd2725ce41943317bfe1ae907c43ef747a
taegyung <taegyung.e@gmail.com>
2026-06-14 21:28:43 +0900

    chore: formatTimeToString 함수 수정

--- src/features/trips/tripSchedule/utils/formatTimeToString.ts
+++ src/features/trips/tripSchedule/utils/formatTimeToString.ts
@@ -1,5 +1,5 @@
 export const formatTimeToString = (hour: number | null, min: number | null) => {
   if (hour === null && min === null) return '-';
 
-  return `${String(hour).padStart(2, '0')}:${String(min).padStart(2, '0')}`;
+  return `${String(hour ?? '00').padStart(2, '0')}:${String(min ?? '00').padStart(2, '0')}`;
 };
